0041e42765d80a81c0c45b8038a48b01d4a8d8da
axxessgranted <gkantor123@gmail.com>
2025-12-11 21:22:55 +0900

    add PostPage route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from "./components/Layout";
 import Home from "./routes/Home";
 import Blog from "./routes/Blog";
 import About from "./routes/About";
+import PostPage from "./routes/PostPage";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/about" element={<About />} />
+        <Route path="/posts/:year/:month/:day/:slug" element={<PostPage />} />
       </Routes>
     </Layout>
   );
